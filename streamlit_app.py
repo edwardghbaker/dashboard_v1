be6d915b8c843ed7270581b5c90d663465969198
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2024-08-04 21:58:59 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   pages/PCA.py
    	new file:   pages/graphing.py
    	new file:   pages/map.py
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+pg = st.navigation([st.Page("pages\map.py",title='Kill me',), st.Page("pages\graphing.py"), st.Page("pages\PCA.py")])
+pg.run()
