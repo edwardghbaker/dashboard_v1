ebf8dc200b7ca2b0f111f26d9aff958b0533970e
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2024-08-05 22:38:22 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   requirements.txt
    	renamed:    pages/PCA.py -> sections/PCA.py
    	renamed:    pages/graphing.py -> sections/graphing.py
    	renamed:    pages/map.py -> sections/map.py
    	modified:   streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+
 
 pg = st.navigation([st.Page("pages\map.py",title='Kill me',), st.Page("pages\graphing.py"), st.Page("pages\PCA.py")])
 pg.run()
