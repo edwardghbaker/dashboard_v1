63ba1a895d814d49106bc597f0c86a52e503cb69
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2024-08-18 21:57:53 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   data/GA.geojson
    	new file:   data/GA.qmd
    	modified:   sections/map.py

--- sections/map.py
+++ sections/map.py
@@ -11,6 +11,8 @@ location_df.drop(10, inplace=True)
 project_data = pd.DataFrame(columns=['lat', 'lon'],
                             data=[[-28.4359, -69.5486]])
 
+GA_Path = os.getcwd().split('dashboard_v1')[0]+'dashboard_v1\\data\\GA.geojson'
+
 st.header('Map of the Locations')
 
 col1, col2 = st.columns([3, 1])
@@ -19,11 +21,13 @@ col2.subheader('Layers')
 surface_data = col2.checkbox('Surface Water Data', value=True)
 ground_data = col2.checkbox('Ground Water Data', value=True)
 col2.subheader('Settings')
+names_on_map = col2.checkbox('Show Names', value=False)
+values_for_column_plot = col2.selectbox('Select the column for the column plot', [None]+list(location_df.columns))
 
 # Define a GeoJSON layer
 geojson_layer = pdk.Layer(
     'GeoJsonLayer',
-    data='https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/geojson/vancouver-blocks.json',
+    data=GA_Path,
     pickable=True,
     stroked=False,
     filled=True,
@@ -33,20 +37,87 @@ geojson_layer = pdk.Layer(
     get_radius=200,
 )
 
+# Define scatter layers 
+site_location = pdk.Layer(
+    'ScatterplotLayer',
+    data=project_data,
+    get_position='[lon, lat]',
+    get_color='[0, 0, 255, 160]',
+    get_radius=500,
+    marker='star',
+)
+
+surface_layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=location_df[location_df['Type'].str.contains('Surface')],
+    get_position='[lon, lat]',
+    get_color='[200, 30, 0, 160]',
+    get_radius=200,
+)
+ground_layer = pdk.Layer(
+    'ScatterplotLayer',
+    data=location_df[location_df['Type'].str.contains('Ground')],
+    get_position='[lon, lat]',
+    get_color='[30, 200, 0, 160]',
+    get_radius=200,
+)
+
+if names_on_map:
+
+    name_layer = pdk.Layer(
+        'TextLayer',
+        data=location_df,
+        get_position='[lon, lat]',
+        get_text='Name',
+        get_size=20,
+        get_color='[0, 0, 0, 255]',
+        get_angle=0,
+        get_text_anchor='middle',
+        get_alignment_baseline='center',
+    )
+
+if values_for_column_plot:
+
+    barplot_layer = pdk.Layer(
+        'ColumnLayer',
+        data=location_df,
+        get_position='[lon, lat]',
+        get_elevation=values_for_column_plot,
+        get_color='[200, 30, 0, 160]',
+        get_radius=200,
+    )
+
 # Define the view state
 view_state = pdk.ViewState(
     latitude=location_df['lat'].mean(),
     longitude=location_df['lon'].mean(),
-    zoom=7,
+    zoom=10,
     pitch=50,
 )
+layers_to_plot = [site_location, geojson_layer]
+
+
+if surface_data:
+    layers_to_plot.append(surface_layer)
+if ground_data:
+    layers_to_plot.append(ground_layer)
+if names_on_map:
+    layers_to_plot.append(name_layer)
+if values_for_column_plot:
+    layers_to_plot.append(barplot_layer)
 
 # Render the deck.gl map with the GeoJSON layer
-col1.pydeck_chart(pdk.Deck(layers=[geojson_layer], initial_view_state=view_state))
+col1.pydeck_chart(pdk.Deck(layers=layers_to_plot, initial_view_state=view_state))
 
-if surface_data and ground_data:
-    col1.map(location_df[location_df['Type'].str.contains('Surface|Ground')], latitude='lat', longitude='lon', use_container_width=True, zoom=7)
-elif surface_data:
-    col1.map(location_df[location_df['Type'].str.contains('Surface')], latitude='lat', longitude='lon', use_container_width=True, zoom=7)
-elif ground_data:
-    col1.map(location_df[location_df['Type'].str.contains('Ground')], latitude='lat', longitude='lon', use_container_width=True, zoom=7)
+
+
+
+
+
+
+# if surface_data and ground_data:
+#     col1.map(location_df[location_df['Type'].str.contains('Surface|Ground')], latitude='lat', longitude='lon', use_container_width=True, zoom=7)
+# elif surface_data:
+#     col1.map(location_df[location_df['Type'].str.contains('Surface')], latitude='lat', longitude='lon', use_container_width=True, zoom=7)
+# elif ground_data:
+#     col1.map(location_df[location_df['Type'].str.contains('Ground')], latitude='lat', longitude='lon', use_container_width=True, zoom=7)
