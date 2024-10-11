a911d47e4d2f5380d78f9fda78e4f93c89b63919
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2024-10-10 17:12:29 -0700

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   sections/PCA.py
    	modified:   sections/home.py
    	modified:   streamlit_app.py
    	modified:   util/scratch.py

--- sections/PCA.py
+++ sections/PCA.py
@@ -5,6 +5,8 @@ from util.PCA_func import perform_pca
 import os
 import pandas as pd
 import plotly.express as px
+from io import BytesIO
+
 #%%
 
 swq = pd.read_pickle(os.getcwd().split('dashboard_v1')[0]+'dashboard_v1\\data\\swq.pkl')
@@ -13,8 +15,9 @@ gwq = pd.read_pickle(os.getcwd().split('dashboard_v1')[0]+'dashboard_v1\\data\\g
 #%%
 
 st.header('Principal Component Analysis')
+col1,col2=st.columns([3,1])
+dataset = col2.radio('Select Dataset', ['Ground Water Quality', 'Surface Water Quality', 'Both'])
 
-dataset = st.radio('Select Dataset', ['Ground Water Quality', 'Surface Water Quality', 'Both'])
 
 if dataset == 'Ground Water Quality':
     df = gwq
@@ -24,29 +27,38 @@ elif dataset == 'Both':
     df = pd.concat([gwq, swq], axis=0)
 
 if df is not None:
-    components = st.multiselect('Select the columns to be used as principal components', ['All','Majors','Dissolved']+list(df.columns), default=['Dissolved'])
+    components = col2.multiselect('Select the columns to be used for principal component analysis', ['All','Majors','Dissolved']+list(df.columns), default=['Dissolved'])
+    print(type(components))
+    components = list(components)
 
     if 'All' in components:
-        components = df.columns
+        components.extend(list(df.columns))
     elif 'Majors' in components:
-        components = list(set(['Total Dissolved Solids','Sulphate (Dissolved)','Redox Potential','Nitrate+Nitrite (Dissolved)','Magnesium (Dissolved)','Iron (Dissolved)', 'Hardness (Dissolved)','Copper (Dissolved)','Alkalinity, Carbonate as CaCO3','Arsenic (Dissolved)']+list(df.columns)))
+        components.extend(['Total Dissolved Solids','Sulphate (Dissolved)','Redox Potential','Nitrate+Nitrite (Dissolved)','Magnesium (Dissolved)','Iron (Dissolved)', 'Hardness (Dissolved)','Copper (Dissolved)','Alkalinity, Carbonate as CaCO3','Arsenic (Dissolved)'])
     elif 'Dissolved' in components:
-        components = list(set([i for i in df.columns if '(Dissolved)' in i]+list(df.columns)))
+        components.extend([i for i in df.columns if '(Dissolved)' in i])
 
-    num_pcs = st.number_input('Select the number of Principal Components to be calculated', min_value=1, max_value=len(components), value=3, step=1)
-    pcs = st.multiselect('Select Principal Components to be plotted', [i for i in range(1, int(num_pcs)+1)])
+    components = list(set(components))
+    components = [i for i in components if i in df.columns]
+    
+    num_pcs = col2.number_input('Select the number of Principal Components to be calculated', min_value=1, max_value=len(components), value=3, step=1)
+    pcs = col2.multiselect('Select Principal Components to be plotted', [i for i in range(1, int(num_pcs)+1)])
 
     principal_df, vectors, X_recreated = perform_pca(df, components, pcs, plot=False)
 
-
     if len(pcs) == 1:
-        st.plotly_chart(px.histogram(principal_df, x=f'PC{pcs[0]}'), use_container_width=True)
+        col1.plotly_chart(px.histogram(principal_df, x=f'PC{pcs[0]}'), use_container_width=True)
     elif len(pcs) == 2:
-        st.plotly_chart(px.scatter(principal_df, x=f'PC{pcs[0]}', y=f'PC{pcs[1]}'), use_container_width=True)
+        col1.plotly_chart(px.scatter(principal_df, x=f'PC{pcs[0]}', y=f'PC{pcs[1]}'), use_container_width=True)
     elif len(pcs) == 3:
-        st.plotly_chart(px.scatter_3d(principal_df,x=f'PC{pcs[0]}', y=f'PC{pcs[1]}', z=f'PC{pcs[2]}'), use_container_width=True)
+        col1.plotly_chart(px.scatter_3d(principal_df,x=f'PC{pcs[0]}', y=f'PC{pcs[1]}', z=f'PC{pcs[2]}'), use_container_width=True)
 
-
-
+    st.write('---')
+    st.write(pd.Series(components,name='Analyte'))
+    st.write('---')
+    st.dataframe(df)
+    st.dataframe(principal_df)
+    st.dataframe(X_recreated)
+    st.dataframe(vectors)
 
 # %%
